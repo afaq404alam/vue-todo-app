754b257b0b99b040b887a1a3dbc2717d28437480
afaq.ansari <afaq.ansari@flixbus.com>
2019-03-05 06:39:01 +0100

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,20 @@
 <template>
   <div id="app">
     <Header/>
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodos"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
-import { constants } from 'fs';
-
-
+import Header from './components/layout/Header'
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: [
-      ]
-    }
-  },
-  methods: {
-    deleteTodos(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.todos = this.todos.filter(t => t.id != id))
-      .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const {title, completed} = newTodo;
-      axios.post("https://jsonplaceholder.typicode.com/todos", {
-        title,
-        completed
-      })
-      .then(res => this.todos = [...this.todos, res.data])
-      .catch(err => console.log(err))
-      this.todos = [...this.todos, newTodo];
-    }
-  },
-  created() {
-    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err));
+    Header
   }
 }
 </script>
+
 
 <style>
 /* #app {
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is a Todo app created using Vue</h1>
+  </div>
+</template>
