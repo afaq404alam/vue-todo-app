4ce934c87596f996a3230c35fd99d7684acc8a6c
afaq.ansari <afaq.ansari@flixbus.com>
2019-03-05 06:46:07 +0100

    Change AddTodo button title

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,7 +2,7 @@
     <div>
         <form @submit="addTodo">
             <input type="text" v-model="title" name="title" placeholder="Add todo...">
-            <input type="submit" value="Submit" class="btn">
+            <input type="submit" value="Add" class="btn">
         </form>
     </div>
 </template>
